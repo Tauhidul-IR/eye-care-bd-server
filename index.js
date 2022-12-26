f9743840a28328382733ef71c27d6282b91a4c93
Tauhidul Islam Robin <robin13690@gmail.com>
2022-12-26 20:57:15 +0600

    booking doctor

--- index.js
+++ index.js
@@ -24,19 +24,55 @@ async function run() {
     try {
         doctorsCollection = client.db('Eye-care-bd').collection('allDoctors');
         servicesCollection = client.db('Eye-care-bd').collection('services');
+        doctorBookingsCollection = client.db('Eye-care-bd').collection('doctorBookings');
 
 
         //all doctors
         app.get('/allDoctors', async (req, res) => {
+            const date = req.query.date;
+            console.log(date);
             const query = {};
-            const doctor = await doctorsCollection.find(query).toArray();
-            res.send(doctor)
+            const doctors = await doctorsCollection.find(query).toArray();
+            const bookingQuery = {
+                appointmentDate: date
+            }
+            const alreadyBooked = await doctorBookingsCollection.find(bookingQuery).toArray()
+            doctors.forEach(doctor => {
+                doctorBooked = alreadyBooked.filter(book => book.doctorName === doctor.name)
+                const bookedSlots = doctorBooked.map(book => book.slot)
+                const remainingSlots = doctor.slots.filter(slot => !bookedSlots.includes(slot))
+                doctor.slots = remainingSlots;
+                console.log(date, doctor.name, remainingSlots.length);
+            })
+            res.send(doctors)
         })
+
+
         //Show 3 doctors
         app.get('/show3Doctors', async (req, res) => {
             const query = {};
             const doctor = await doctorsCollection.find(query).limit(3).toArray();
             res.send(doctor)
+        })
+
+
+        //doctor booking appointment
+        app.post('/bookingDoctor', async (req, res) => {
+            const booking = req.body
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                doctorName: booking.doctorName
+            }
+
+            const alreadyBooked = await doctorBookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
+            const result = await doctorBookingsCollection.insertOne(booking);
+            res.send(result);
         })
     }
     finally {
